c04d3cefaedc4fcea2f580d8731ea91ab26650a0
yuting <s830s200s@gmail.com>
2022-12-23 14:47:39 +0800

    準備做系統大整改

--- test/read_cpp.py
+++ test/read_cpp.py
@@ -9,12 +9,15 @@ trigger_idx=0
 
 ##### ASF #####
 asf_node = arr_phaser.get(1).get(3)
-print(''.join(asf_node.get(trigger_idx).get(1).reconstruct()))
+print(''.join(asf_node.get(trigger_idx).get(9).reconstruct()))
 
-for i in range(len(asf_node.get(trigger_idx).get(0).pos_arr)):
-    asf_node.get(trigger_idx).get(1).get(i).text = str(i)
+# for i in range(len(asf_node.get(trigger_idx).get(0).pos_arr)):
+#     # asf_node.get(trigger_idx).get(1).get(i).text = str(i)
+#     print(''.join(asf_node.get(trigger_idx).get(9).reconstruct()))
 
-print(''.join(asf_node.get(trigger_idx).get(0).reconstruct()))
+
+
+# print(''.join(asf_node.get(trigger_idx).get(0).reconstruct()))
 # path = 'chromatix_hi556_j3_main_snapshot_cpp_modify.h'
 # with open(path, 'w', encoding='cp1252') as f:
 #     f.write(''.join(arr_phaser.reconstruct()))
@@ -29,7 +32,7 @@ print(''.join(asf_node.get(trigger_idx).get(0).reconstruct()))
 ##### ASF #####
 
 ##### WNR #####
-asf_node = arr_phaser.get(4).get(3)
-print(''.join(asf_node.get(trigger_idx).get(1).reconstruct()))
+# asf_node = arr_phaser.get(4).get(3)
+# print(''.join(asf_node.get(trigger_idx).get(1).reconstruct()))
 
 ##### WNR #####
